07f74f92df42e83dee322405822a5a6fbbb023c5
Kameron Ferrer <32345507+kickingdude@users.noreply.github.com>
2024-01-19 10:24:17 -0800

    Added Config Files

--- Natural Selection/Source Code/DataAccess/DataAccessCraftVerify/IReadOnlyDAO.cs
+++ Natural Selection/Source Code/DataAccess/DataAccessCraftVerify/IReadOnlyDAO.cs
@@ -2,6 +2,6 @@
 {
     public interface IReadOnlyDAO
     {
-        public ICollection<object>? GetAttribute(string connString, string sqlcommand);
+        public ICollection<object>? GetAttribute(string sqlcommand);
     }
 }
--- Natural Selection/Source Code/DataAccess/DataAccessLibraryCraftVerifyTests/SQLServerDAOTests.cs
+++ Natural Selection/Source Code/DataAccess/DataAccessLibraryCraftVerifyTests/SQLServerDAOTests.cs
@@ -14,23 +14,21 @@ namespace DataAccessLibraryCraftVerify.Tests
         [TestMethod()]
         public void InsertAttributeTest()
         {
-            string SqlServerconnString = "Server=localhost;Database=CraftVerify;User Id=admin;Password=admin;TrustServerCertificate=true";
             //string MySqlServerconnString = "@Server=myServerAddress;Database=myDataBase;User=myUsername;Password=myPassword;\r\n";
 
             string INSERTsqlcommand = "INSERT INTO ClaimPrinciple (claim, stuff, hashPrinciple) VALUES ('admin', 'admin', 'aikjwnrvikhjqb3nrb');";
             SQLServerDAO sqlDAO = new SQLServerDAO();
 
-            Assert.IsTrue((sqlDAO.InsertAttribute(SqlServerconnString, INSERTsqlcommand)) == 1);
+            Assert.IsTrue((sqlDAO.InsertAttribute(INSERTsqlcommand)) == 1);
         }
 
         [TestMethod()]
         public void GetAttributeTest()
         {
-            string SqlServerconnString = "Server=localhost;Database=CraftVerify;User Id=admin;Password=admin;TrustServerCertificate=true";
             string GETsqlcommand = "SELECT * FROM ClaimPrinciple WHERE claim = 'admin';";
             SQLServerDAO sqlDAO = new SQLServerDAO();
-            Console.WriteLine(sqlDAO.GetAttribute(SqlServerconnString, GETsqlcommand));
-            Assert.IsTrue(sqlDAO.GetAttribute(SqlServerconnString, GETsqlcommand) != null);
+            Console.WriteLine(sqlDAO.GetAttribute(GETsqlcommand));
+            Assert.IsTrue(sqlDAO.GetAttribute(GETsqlcommand) != null);
         }
     }
 }
